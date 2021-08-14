b747b7a459c226c3dedb827c1600af2e74d23be6
Samuel Gómez <samuelgomez@seven4n.com>
2021-08-14 18:34:46 -0500

    Commit inicial

---
+++ module1/src/main/java/Application1.java
@@ -0,0 +1,8 @@
+import io.micronaut.runtime.Micronaut;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application1.class, args);
+    }
+}
---
+++ module1/src/main/java/controllers/ValidateMutanteRequestDTO.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.micronaut.core.annotation.Introspected;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Introspected
+public class ValidateMutanteRequestDTO {
+    @JsonProperty("dna")
+    private List<String> dna = new ArrayList<>();
+
+    public ValidateMutanteRequestDTO(){}
+    public List<String> getDna() {
+        return dna;
+    }
+    public void setDna(List<String> dna) {
+        this.dna = dna;
+    }
+    public ValidateMutanteRequestDTO(List<String> dna) {
+        this.dna = dna;
+    }
+}
